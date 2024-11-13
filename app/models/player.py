2b5b1deb9edef7259b3fc37c3792a1310fab707c
SwthSawTheme <sawtheme1@gmail.com>
2024-11-12 21:05:08 -0300

    Conceito class player definido

--- app/models/player.py
+++ app/models/player.py
@@ -3,5 +3,20 @@
 class Player(object):
 
 
-    def __init__(self):
+    def __init__(self,name:str,health:int,damage:int,arrows:int):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.arrows = arrows
+
+    def attack(self,mob):
+        pass
+
+    def collectArrows(self,qty:int):
+        pass
+
+    def takeDamage(self,value: int):
+        pass
+
+    def isAlive(self) -> bool:
         pass
